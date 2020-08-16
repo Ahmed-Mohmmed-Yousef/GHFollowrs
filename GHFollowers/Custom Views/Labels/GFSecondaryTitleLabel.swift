c7f5d38c06358aa52b3c2804adf2fabbaff51344
Ahmed Mohamed Yousef <a41155098@gmail.com>
2020-08-16 09:21:36 +0200

    GFUserInfoHeaderVC

---
+++ GHFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Ahmed on 8/15/20.
+//  Copyright © 2020 Ahmed,ORG. All rights reserved.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    fileprivate func configure(){
+        textColor                   = .secondaryLabel
+        adjustsFontSizeToFitWidth   = true
+        minimumScaleFactor          = 0.90
+        lineBreakMode               = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Ahmed on 8/15/20.
+//  Copyright © 2020 Ahmed,ORG. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
