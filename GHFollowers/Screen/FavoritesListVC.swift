a46b957717e7293d3f40b83038986639f290c755
Ahmed Mohamed Yousef <a41155098@gmail.com>
2020-08-19 20:08:43 +0200

    table view for Favorites List VC

--- GHFollowers/Screen/FavoritesListVC.swift
+++ GHFollowers/Screen/FavoritesListVC.swift
@@ -9,22 +9,96 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
-
+    
+    let tableView               = UITableView()
+    var favorites: [Follower]   = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
-        
-        PersistenceManager.retrieveFavorites { (result) in
+        configureViewController()
+        configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    private func configureViewController() {
+        view.backgroundColor    = .systemBackground
+        title                   = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame         = view.bounds
+        tableView.rowHeight     = 80
+        tableView.delegate      = self
+        tableView.dataSource    = self
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
+    }
+
+    private func getFavorites() {
+        PersistenceManager.retrieveFavorites {[weak self](result) in
+            guard let self = self else { return }
             switch result {
-                
             case .success(let favorites):
-                print(favorites)
-            case .failure(_):
-                break
+                
+                if favorites.isEmpty {
+                    self.showEmtyStateView(with: "No Favorites?\nAdd one on follower screen.", in: self.view)
+                } else {
+                    self.favorites = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
+                
+                
+            case .failure(let error):
+                self.presentAlertOnMainThread(title: "Somthing went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
     
+}
 
+extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID, for: indexPath) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite    = favorites[indexPath.row]
+        let destVC      = FolloweListVC()
+        destVC.username = favorite.login
+        destVC.title    = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let favorite    = favorites[indexPath.row]
+        if editingStyle == .delete {
+            PersistenceManager.update(favorite: favorite, actionType: .remove) {[weak self] (error) in
+                guard let self = self else { return }
+                if let error = error {
+                    self.presentAlertOnMainThread(title: "Somthing went wrong", message: error.rawValue, buttonTitle: "Ok")
+                } else {
+                    self.favorites.remove(at: indexPath.row)
+                    self.tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+            }
+        }
+    }
 }
