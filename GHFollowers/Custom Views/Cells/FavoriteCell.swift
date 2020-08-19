3a2b7c84942f7c89571aea28b4f8f269e28a3a72
Ahmed Mohamed Yousef <a41155098@gmail.com>
2020-08-19 11:30:58 +0200

    Favorite cell

---
+++ GHFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,51 @@
+//
+//  FavoriteCell.swift
+//  GHFollowers
+//
+//  Created by Ahmed on 8/19/20.
+//  Copyright © 2020 Ahmed,ORG. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+    
+    static let reuseID = "FollowerCell"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLable(textAlignment: .left, fontSize: 26)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(favorite: Follower){
+        usernameLabel.text = favorite.login
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+    }
+    
+    fileprivate func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType           = .disclosureIndicator
+        let padding: CGFloat    = 12.0
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+}
